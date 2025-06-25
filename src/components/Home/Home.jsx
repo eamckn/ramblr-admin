d2b3b9bcdab48dadd3a9165ecbd422426bc8c46f
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-06-25 17:35:33 -0400

    feat: add previews to home when admin is logged in

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,26 @@
+import { useOutletContext } from "react-router-dom"
+import Preview from "../Preview/Preview"
+import styles from './Home.module.css'
+
 const Home = () => {
 
+    const { postsData }  = useOutletContext()
+
     return (
-        <>
-            Home!
-        </>
+        <div id={styles.home}>
+            <h1 className={styles.title}>Home</h1>
+            <div id={styles.previewDisplay}>
+                {postsData.map((post, index) => {
+                    return <Preview key={post.id}
+                        id={post.id}
+                        title={post.title}
+                        content={post.content}
+                        timestamp={post.postedAt}
+                        commentCount={post.comments.length}
+                        animationOrder={index + 1} />
+                })}
+            </div>
+        </div>
     )
 
 }
---
+++ src/components/Preview/Preview.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom"
+import styles from './Preview.module.css'
+
+const Preview = ({ id, title, content, timestamp, commentCount, animationOrder }) => {
+
+    const slicedContent = content.slice(0, 100)
+
+    const contentPreview = slicedContent.slice(-1) === ' ' ? (
+        slicedContent.slice(0, -1) + '...'
+    ) : (
+            slicedContent + '...'
+        )
+    
+    function formatDate(timestamp) {
+        const date = new Date(timestamp)
+
+        const month = date.toLocaleString('default', { month: 'long' });
+        const day = String(date.getDate()).padStart(2, '0');
+        const year = date.getFullYear();
+
+        return `${month} ${day}, ${year}`;
+    }
+
+    return (
+        <div className={styles.preview} style={{ '--animation-order': `${animationOrder}`}}>
+            <h2 className={styles.title}>{title}</h2>
+            <p className={styles.content}>{contentPreview}</p>
+            <div className={styles.dateComments}>
+                Posted on {formatDate(timestamp)} • {commentCount} comments
+            </div>
+            <Link to={'/posts/' + id} className={styles.readMore}>
+                Read more
+            </Link>
+        </div>
+    )
+
+}
+
+export default Preview
