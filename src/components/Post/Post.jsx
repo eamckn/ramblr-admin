1e52ac025b699f724b59712670918daa8679d7dc
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-09-22 14:05:13 -0400

    feat: change published state on button click

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,32 @@
-import { Link, useParams, useOutletContext } from "react-router-dom"
+import { Link, useParams, useOutletContext, useNavigate } from "react-router-dom"
 import styles from './Post.module.css'
 
 const Post = () => {
 
     const { postId } = useParams();
     const { postsData } = useOutletContext()
+    const navigate = useNavigate()
 
     const postData = postsData.find(post => 
             post.id === Number(postId)
     )
+
+    const handlePublishClick = async () => {
+        const token = localStorage.getItem('ramblrAdminJWT');
+        const authHeader = 'Bearer ' + token;
+        const paramString = postData.isPublished ? new URLSearchParams({
+            unpublish: 't'
+        }).toString() : new URLSearchParams({
+            publish: 't'
+        }).toString();
+        await fetch(`http://localhost:8080/posts/${postData.id}?` + paramString, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `${authHeader}`
+            },
+        })
+        navigate('/')
+    }
         
     return (
         <div className={styles.postComments}>
@@ -16,7 +34,13 @@ const Post = () => {
                 <h1 className={styles.title}>{postData.title}</h1>
                 <p className={styles.content}>{postData.content}</p>
                 <div className={styles.options}>
-                    <button className={styles.edit}>Edit</button>
+                    <Link to='./edit'>
+                        <button className={styles.edit}>Edit</button>
+                    </Link>
+                    <button className={styles.published}
+                        onClick={handlePublishClick}>
+                        {postData.isPublished ? 'Unpublish' : 'Publish'}
+                    </button>
                 </div>
             </div>
         </div>
