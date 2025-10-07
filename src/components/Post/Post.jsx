f3d327c66aeb5ce1252935fb1ff5e41735168dc8
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-10-07 11:46:59 -0400

    feat: enable editing for post content and title

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,4 +1,5 @@
-import { Link, useParams, useOutletContext, useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useParams, useOutletContext, useNavigate } from "react-router-dom"
 import styles from './Post.module.css'
 
 const Post = () => {
@@ -10,6 +11,19 @@ const Post = () => {
     const postData = postsData.find(post => 
             post.id === Number(postId)
     )
+
+    const [postTitle, setPostTitle] = useState(postData.title)
+    const [postContent, setPostContent] = useState(postData.content)
+
+    const handleTitleBlur = (e) => {
+        e.preventDefault()
+        setPostTitle(e.target.textContent)
+    }
+
+    const handleContentBlur = (e) => {
+        e.preventDefault()
+        setPostContent(e.target.innerHTML)
+    }
 
     const handlePublishClick = async () => {
         const token = localStorage.getItem('ramblrAdminJWT');
@@ -27,16 +41,41 @@ const Post = () => {
         })
         navigate('/')
     }
+
+    const handleSaveClick = async () => {
+        const token = localStorage.getItem('ramblrAdminJWT');
+        const authHeader = 'Bearer ' + token;
+        const response = await fetch(`http://localhost:8080/posts/${postData.id}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `${authHeader}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: postTitle,
+                content: postContent
+            })
+        })
+        const json = await response.json()
+        console.log(json)
+    }
         
     return (
         <div className={styles.postComments}>
             <div className={styles.post}>
-                <h1 className={styles.title}>{postData.title}</h1>
-                <p className={styles.content}>{postData.content}</p>
+                <h1 className={styles.title}
+                    onBlur={handleTitleBlur}
+                    contentEditable='plaintext-only'
+                    suppressContentEditableWarning={true} >
+                    {postData.title}
+                </h1>
+                <article className={styles.content}
+                    onBlur={handleContentBlur}
+                    contentEditable
+                    dangerouslySetInnerHTML={{__html: postContent}}
+                    suppressContentEditableWarning={true} />
                 <div className={styles.options}>
-                    <Link to='./edit'>
-                        <button className={styles.edit}>Edit</button>
-                    </Link>
+                    <button className={styles.save} onClick={handleSaveClick}>Save changes</button>
                     <button className={styles.published}
                         onClick={handlePublishClick}>
                         {postData.isPublished ? 'Unpublish' : 'Publish'}
