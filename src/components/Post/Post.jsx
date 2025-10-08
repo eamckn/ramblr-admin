8b08f247baa77547aeea9d5836883e47a08fd3ee
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-10-08 13:56:38 -0400

    feat: retrieve new data on save or publish click

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -5,7 +5,7 @@ import styles from './Post.module.css'
 const Post = () => {
 
     const { postId } = useParams();
-    const { postsData } = useOutletContext()
+    const { postsData, getData } = useOutletContext()
     const navigate = useNavigate()
 
     const postData = postsData.find(post => 
@@ -14,6 +14,7 @@ const Post = () => {
 
     const [postTitle, setPostTitle] = useState(postData.title)
     const [postContent, setPostContent] = useState(postData.content)
+    const [editError, setEditError] = useState(null)
 
     const handleTitleBlur = (e) => {
         e.preventDefault()
@@ -26,63 +27,82 @@ const Post = () => {
     }
 
     const handlePublishClick = async () => {
-        const token = localStorage.getItem('ramblrAdminJWT');
-        const authHeader = 'Bearer ' + token;
-        const paramString = postData.isPublished ? new URLSearchParams({
-            unpublish: 't'
-        }).toString() : new URLSearchParams({
-            publish: 't'
-        }).toString();
-        await fetch(`http://localhost:8080/posts/${postData.id}?` + paramString, {
-            method: 'PUT',
-            headers: {
-                'Authorization': `${authHeader}`
-            },
-        })
-        navigate('/')
+        try {
+            const token = localStorage.getItem('ramblrAdminJWT');
+            const authHeader = 'Bearer ' + token;
+            const paramString = postData.isPublished ? new URLSearchParams({
+                unpublish: 't'
+            }).toString() : new URLSearchParams({
+                publish: 't'
+            }).toString();
+            const response = await fetch(`http://localhost:8080/posts/${postData.id}?` + paramString, {
+                method: 'PUT',
+                headers: {
+                    'Authorization': `${authHeader}`
+                },
+            })
+            if (!response.ok) {
+                setEditError(new Error('Server error'))
+            } else {
+                await getData()
+                navigate('/')
+            }
+        } catch (error) {
+            setEditError(error)
+        }
     }
 
     const handleSaveClick = async () => {
-        const token = localStorage.getItem('ramblrAdminJWT');
-        const authHeader = 'Bearer ' + token;
-        const response = await fetch(`http://localhost:8080/posts/${postData.id}`, {
-            method: 'PUT',
-            headers: {
-                'Authorization': `${authHeader}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                title: postTitle,
-                content: postContent
+        try {
+            const token = localStorage.getItem('ramblrAdminJWT');
+            const authHeader = 'Bearer ' + token;
+            const response = await fetch(`http://localhost:8080/posts/${postData.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Authorization': `${authHeader}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title: postTitle,
+                    content: postContent
+                })
             })
-        })
-        const json = await response.json()
-        console.log(json)
+            if (!response.ok) {
+                setEditError(new Error('Server error'))
+            } else {
+                await getData()
+            }
+        } catch (error) {
+            setEditError(error)
+        }
     }
         
     return (
-        <div className={styles.postComments}>
-            <div className={styles.post}>
-                <h1 className={styles.title}
-                    onBlur={handleTitleBlur}
-                    contentEditable='plaintext-only'
-                    suppressContentEditableWarning={true} >
-                    {postData.title}
-                </h1>
-                <article className={styles.content}
-                    onBlur={handleContentBlur}
-                    contentEditable
-                    dangerouslySetInnerHTML={{__html: postContent}}
-                    suppressContentEditableWarning={true} />
-                <div className={styles.options}>
-                    <button className={styles.save} onClick={handleSaveClick}>Save changes</button>
-                    <button className={styles.published}
-                        onClick={handlePublishClick}>
-                        {postData.isPublished ? 'Unpublish' : 'Publish'}
-                    </button>
+        <>
+            <div className={styles.postComments}>
+                <div className={styles.post}>
+                    <h1 className={styles.title}
+                        onBlur={handleTitleBlur}
+                        contentEditable='plaintext-only'
+                        suppressContentEditableWarning={true} >
+                        {postData.title}
+                    </h1>
+                    <article className={styles.content}
+                        onBlur={handleContentBlur}
+                        contentEditable
+                        dangerouslySetInnerHTML={{__html: postContent}}
+                        suppressContentEditableWarning={true} />
+                    <div className={styles.options}>
+                        <button className={styles.save} onClick={handleSaveClick}>Save changes</button>
+                        <button className={styles.published}
+                            onClick={handlePublishClick}>
+                            {postData.isPublished ? 'Unpublish' : 'Publish'}
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+            {editError && <div className={styles.error}>There was an error trying to make your edits. Please try again.</div>}
+        </>
     )
 }
 
